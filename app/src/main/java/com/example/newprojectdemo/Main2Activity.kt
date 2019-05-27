960b52c3d4f1f2a40adec971cce23374d99f08ee
ZXCloud <981014xue>
2019-05-27 10:00:45 +0800

    Second Commit

--- app/src/main/java/com/example/newprojectdemo/Main2Activity.kt
+++ app/src/main/java/com/example/newprojectdemo/Main2Activity.kt
@@ -9,5 +9,6 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
     }
 }
