aa6ced0c0a581acc71495d7f39162db45ca935d1
ZXCloud <981014xue>
2019-05-27 09:53:38 +0800

    first Commit

---
+++ app/src/main/java/com/example/newprojectdemo/Main2Activity.kt
@@ -0,0 +1,13 @@
+package com.example.newprojectdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Main2Activity : AppCompatActivity() {
+    internal var TexT: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
